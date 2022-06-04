edec693d486ccd86db3e4ce5b32ea9c606f54b08
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-06-04 13:49:40 +0300

    Initial Commit

---
+++ WhileAndPluffy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhileAndPluffy
+//
+//  Created by Kirill Drozdov on 04.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
